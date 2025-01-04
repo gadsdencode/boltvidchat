500922bc72dc0f617e79a9790dabe1bf56831343
gadsdencode <jordan.martens@osscontact.com>
2025-01-04 07:35:06 -0500

    added 3D animated background with collision detection and bounce effect

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './index.css';
 import VideoChat from './components/VideoChat';
 import { AuthProvider } from './contexts/AuthContext';
+import AnimatedBackground from './components/AnimatedBackground';
 
 function App() {
   return (
     <AuthProvider>
       <div className="App">
+        <AnimatedBackground />
         <VideoChat />
       </div>
     </AuthProvider>
---
+++ src/components/AnimatedBackground.tsx
@@ -0,0 +1,159 @@
+import React from 'react';
+
+const AnimatedBackground: React.FC = () => {
+  return (
+    <div className="animated-background">
+      <div className="perspective-container">
+        {/* First 3D shape group */}
+        <div className="shape-container shape-container-1">
+          <div className="shape-transformer transformer-1">
+            <div className="shape-fragment fragment-1">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-2">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-3">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* Second 3D shape group */}
+        <div className="shape-container shape-container-2">
+          <div className="shape-transformer transformer-2">
+            <div className="shape-fragment fragment-1">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-2">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-3">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* Third 3D shape group */}
+        <div className="shape-container shape-container-3">
+          <div className="shape-transformer transformer-3">
+            <div className="shape-fragment fragment-1">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-2">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-3">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* Decorative 3D shapes */}
+        <div className="shape-container shape-container-4">
+          <div className="shape-transformer transformer-4">
+            <div className="shape-fragment fragment-1">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-2">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-3">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+          </div>
+        </div>
+        <div className="shape-container shape-container-5">
+          <div className="shape-transformer transformer-5">
+            <div className="shape-fragment fragment-1">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-2">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+            <div className="shape-fragment fragment-3">
+              <div className="face front"></div>
+              <div className="face back"></div>
+              <div className="face left"></div>
+              <div className="face right"></div>
+              <div className="face top"></div>
+              <div className="face bottom"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedBackground; 
